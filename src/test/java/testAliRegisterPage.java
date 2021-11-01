c19f0c4ec50a359da7b531c33d859bcd7a08bf31
GladosRae <93376985+GladosRae@users.noreply.github.com>
2021-11-01 15:22:50 +0500

    Update testAliRegisterPage.java

--- src/test/java/testAliRegisterPage.java
+++ src/test/java/testAliRegisterPage.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -16,11 +17,11 @@ public class testAliRegisterPage {
 
     @Before
     public void setUp() throws MalformedURLException {
-        /*System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver.exe");
-        driver = new FirefoxDriver();*/
+        System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
 
-        DesiredCapabilities caps = DesiredCapabilities.firefox();
-        driver = new RemoteWebDriver(new URL("localhost:4444/wd/hub"), caps);
+        /*DesiredCapabilities caps = DesiredCapabilities.firefox();
+        driver = new RemoteWebDriver(new URL("localhost:4444/wd/hub"), caps);*/
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://login.aliexpress.ru/?flag=1&return_url=https%3A%2F%2Fhome.aliexpress.ru%2Findex.htm%3Fspm%3Da2g2w.home.1000001.9.75df5430uSVwgI%26tracelog%3Dws_topbar%26_ga%3D2.195076588.1321411370.1634223558-767497661.1634026945");
     }
